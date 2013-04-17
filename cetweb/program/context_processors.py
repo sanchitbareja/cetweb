f44fea436a8df8142a7a17a00ee85f2675663010
Henry Bradlow <hbradlow@berkeley.edu>
2013-04-16 17:35:01 -0700

    Fixed form

--- cetweb/program/context_processors.py
+++ cetweb/program/context_processors.py
@@ -1,3 +1,8 @@
 from program.models import Program
+from program.forms import CertificateApplicationForm
 def programs(request):
     return {"programs":Program.objects.all()}
+
+def apply_form(request):
+    certificate_form = CertificateApplicationForm()
+    return {'certificate_form':certificate_form}
